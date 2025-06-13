dfbba71a8a9f0e3eb8190d52111f7f13d28a50b7
varunnamala <vxn220039@utdallas.edu>
2025-06-13 16:19:00 -0500

    updated install.sh to include python and Node.js

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "📦 Updating packages..."
+sudo apt update -y
+
+echo "🐍 Installing Python..."
+sudo apt install python3 -y
+
+echo "⬇️ Installing Node.js and npm..."
+sudo apt install nodejs npm -y
+
+echo "✅ Installed Versions:"
+echo -n "Java: " && java -version | head -n 1
+echo -n "Python: " && python3 --version
+echo -n "Node.js: " && nodejs --version
+echo -n "npm: " && npm --version
